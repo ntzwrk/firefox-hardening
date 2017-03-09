f3c03ab4412dcb6e5b1796b39c6be5228ca01831
Valentin Brandl <vbrandl@riseup.net>
2017-03-09 20:56:00 +0100

    Actually add the updated file...

--- user-settings.sh
+++ user-settings.sh
@@ -1,25 +1,31 @@
 #!/usr/bin/env bash
-# This script writes some privacy settings into a firefox profile
-# The `prefs.js` file inside a firefox profile direcotry stores
+# This script writes some privacy settings into a Firefox profile
+# The `prefs.js` file inside a Firefox profile directory stores
 # user defined settings.
 # created: 2017-03-09
 # author: Valentin Brandl
 
-# exit on error
-set -e
-
-# find the default firefox profile
+# find the default Firefox profile
 function find_default_profile {
-	source <(grep = ${HOME}/.mozilla/firefox/profiles.ini)
-	echo "${HOME}/.mozilla/firefox/${Path}/"
+	if [ -e ${HOME}/.mozilla/firefox/profiles.ini ]
+	then
+		# hacky way to read settings from profile.ini since the format is
+		#   Key=Value
+		# just like in bash
+		# TODO: do better (ini parser?)
+		source <(grep = ${HOME}/.mozilla/firefox/profiles.ini)
+		echo "${HOME}/.mozilla/firefox/${Path}/"
+	else
+		echo ""
+	fi
 }
 
-# write a setting to the firefox profile
+# write a setting to the Firefox profile
 # params: setting_name, value, type
 # type has to be "string", "bool" or "int"
 function write_setting {
 	local setting="user_pref(\"${1}\", "
-	case ${3} in
+	case "${3}" in
 		string)
 			setting="${setting}\"${2}\");"
 			;;
@@ -33,24 +39,33 @@ function write_setting {
 	echo ${setting} | tee -a ${PROFILE_PATH}/prefs.js
 }
 
-function usage {
+function print_version {
 	echo "${NAME} ${VERSION}"
+}
+
+function usage {
+	print_version
 	echo "Usage:"
 	echo "  -h | --help"
 	echo "    Print this help message"
 	echo "  -p | --profile"
-	echo "    Path to the firefox profile"
+	echo "    Path to the Firefox profile"
 	echo "    Default: extracted from \$HOME/.mozilla/firefox/profiles.ini"
+	echo "  -v | --version"
+	echo "    Print version and exit"
 	exit 0
 }
 
-NAME="Firefox privacy script"
+NAME="Firefox user settings script"
 VERSION=0.1
-PROFILE_PATH=$(find_default_profile)
-SHORT_OPTIONS="hp:"
-LONG_OPTIONS="help,profile:"
+SHORT_OPTIONS="hp:v"
+LONG_OPTIONS="help,profile:,version"
 
 params="$(getopt -o ${SHORT_OPTIONS} -l ${LONG_OPTIONS} -- "${@}")"
+
+# exit on error
+# set this after getopt so the usage dialog gets printed if getopt fails
+set -e
 
 if [ $? -ne 0 ]
 then
@@ -60,15 +75,21 @@ fi
 eval set -- "${params}"
 unset params
 
+PROFILE_PATH=$(find_default_profile)
+
 while true
 do
-	case $1 in
+	case "$1" in
 		-h|--help)
 			usage
 			;;
 		-p|--profile)
 			PROFILE_PATH=(${2-})
 			shift 2
+			;;
+		-v|--version)
+			print_version
+			exit
 			;;
 		--)
 			shift
@@ -79,6 +100,13 @@ do
 			;;
 	esac
 done
+
+if [ "${PROFILE_PATH}" == "" ]
+then
+	echo "Could not find default profile. Please use -p flag"
+	echo "You might need to run Firefox at least once to create a profile"
+	exit 1
+fi
 
 write_setting "geo.enabled" "false" "bool"
 # vim: set filetype=sh ts=4 sw=4 tw=0 noet :
