329b9649c03b872a707e94377e9612052f6b46dc
Valentin Brandl <vbrandl@riseup.net>
2017-03-09 23:29:41 +0100

    Add some privacy settings

--- user-settings.sh
+++ user-settings.sh
@@ -108,5 +108,50 @@ then
 	exit 1
 fi
 
+# values from https://wiki.archlinux.org/index.php/Firefox/Privacy
+# disable geolocation
 write_setting "geo.enabled" "false" "bool"
+# enable tracking protection
+# https://support.mozilla.org/t5/Protect-your-privacy/Tracking-Protection-in-Firefox/ta-p/18255
+write_setting "privacy.trackingprotection.enabled" "true" "bool"
+# set the useragent to the same one used by the Tor browser
+write_setting "general.useragent.override" "Mozilla/5.0 (Windows NT 6.1; rv:38.0) Gecko/20100101 Firefox/38.0" "string"
+# set the platform to the same one used by the Tor browser
+write_setting "general.platform.override" "Win32" "string"
+# disable battery api
+write_setting "dom.battery.enabled" "false" "bool"
+# disable WebRTC
+write_setting "media.peerconnection.ice.default_address_only" "true" "bool"
+write_setting "media.peerconnection.enabled" "false" "bool"
+# disable 1024-bit dh primes
+write_setting "security.ssl3.dhe_rsa_aes_128_sha" "false" "bool"
+write_setting "security.ssl3.dhe_rsa_aes_256_sha" "false" "bool"
+# disable telemetry
+write_setting "toolkit.telemetry.enabled" "false" "bool"
+# enable Do Not Track
+write_setting "privacy.donottrackheader.enabled" "true" "bool"
+# disable safebrowsing
+write_setting "browser.safebrowsing.malware.enabled" "false" "bool"
+write_setting "browser.safebrowsing.phishing.enabled" "false" "bool"
+# disable WebGL
+write_setting "webgl.disabled" "true" "bool"
+
+
+# disable DNS prefetch
+write_setting "network.dns.disablePrefetch" "true" "bool"
+# disable cookies from 3rd parties
+write_setting "network.cookie.cookieBehavior" "1" "int"
+# keep cookies until Firefox is closed
+write_setting "network.cookie.lifetimePolicy" "2" "int"
+write_setting "network.cookie.prefsMigrated" "true" "bool"
+# do not remember visited links
+write_setting "layout.css.visited_links_enabled" "false" "bool"
+# try resolving DNS over SOCKS proxies (needed for Tor)
+write_setting "network.proxy.socks_remote_dns" "true" "bool"
+# use `localhost` as URL for WiFi locating
+write_setting "geo.wifi.url" "localhost" "string"
+# disable search suggestions
+write_setting "browser.search.suggest.enabled" "false" "bool"
+# do not embed the `missing flash player` frame
+write_setting "plugins.notifyMissingFlash" "false" "bool"
 # vim: set filetype=sh ts=4 sw=4 tw=0 noet :
