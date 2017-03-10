c0b7775400b3c2ca48453a9c15c076e6392a65d3
Felix Singer <singer.felix@t-online.de>
2017-03-10 21:19:49 +0100

    Copy hardened configs to Firefox dir

--- beatthedeviloutofit.sh
+++ beatthedeviloutofit.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 
-SYSTEM_ADDONS_DIR="/usr/lib/firefox/browser/features"
+FF_DIR="/usr/lib/firefox/browser"
+FH_DIR="/usr/lib/firefox-hardening/"
+
+SYSTEM_ADDONS_DIR=${FF_DIR}/features
 SYSTEM_ADDONS="firefox@getpocket.com.xpi webcompat@mozilla.org.xpi aushelper@mozilla.org.xpi"
+
+SYSTEM_PREFS_DIR=${FF_DIR}/defaults/preferences/
 
 for ADDON in ${SYSTEM_ADDONS}; do
 	if [ -f ${SYSTEM_ADDONS_DIR}/${ADDON} ]; then
 		rm ${SYSTEM_ADDONS_DIR}/${ADDON}
 	fi
 done
+
+if [ -d ${SYSTEM_PREFS_DIR} ]; then
+	cp ${FH_DIR}/prefs/* ${SYSTEM_PREFS_DIR}/
+fi
