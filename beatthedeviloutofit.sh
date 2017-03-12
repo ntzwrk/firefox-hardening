2e6e070aeb1e107c6bc76c3631e6bc61ccb09e9c
Felix Singer <singer.felix@t-online.de>
2017-03-12 20:22:05 +0100

    Use soft links instead of config copy

--- beatthedeviloutofit.sh
+++ beatthedeviloutofit.sh
@@ -6,14 +6,19 @@ FH_DIR="/usr/lib/firefox-hardening/"
 SYSTEM_ADDONS_DIR=${FF_DIR}/features
 SYSTEM_ADDONS="firefox@getpocket.com.xpi webcompat@mozilla.org.xpi aushelper@mozilla.org.xpi"
 
-SYSTEM_PREFS_DIR=${FF_DIR}/defaults/preferences/
+SYSTEM_PREFS_DIR=${FF_DIR}/defaults/preferences
+CUSTOM_CONFIGS="99-custom.js"
 
-for ADDON in ${SYSTEM_ADDONS}; do
-	if [ -f ${SYSTEM_ADDONS_DIR}/${ADDON} ]; then
-		rm ${SYSTEM_ADDONS_DIR}/${ADDON}
-	fi
-done
+if [ -d ${FF_DIR} ]; then
+	for ADDON in ${SYSTEM_ADDONS}; do
+		if [ -f ${SYSTEM_ADDONS_DIR}/${ADDON} ]; then
+			rm ${SYSTEM_ADDONS_DIR}/${ADDON}
+		fi
+	done
 
-if [ -d ${SYSTEM_PREFS_DIR} ]; then
-	cp ${FH_DIR}/prefs/* ${SYSTEM_PREFS_DIR}/
+	for CONFIG in ${CUSTOM_CONFIGS}; do
+		if [ ! -L ${SYSTEM_PREFS_DIR}/${CONFIG} ]; then
+			ln -s ${FH_DIR}/prefs/${CONFIG} ${SYSTEM_PREFS_DIR}/${CONFIG}
+		fi
+	done
 fi
