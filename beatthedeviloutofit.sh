494665cc7ea0279e7ac36536d546f1f77d530587
Felix Singer <singer.felix@t-online.de>
2017-03-13 07:43:04 +0100

    Restructure all the things
    If the prefs config file is stored under /usr/lib/firefox/browser/defaults/preferences
    Firefox seems to override some options with its own values. To fix that it's necessary
    to add an additional config file autoconfig.js, where the prefs custom config file is
    defined.
    
    I also added a config file to be more modular.

--- beatthedeviloutofit.sh
+++ beatthedeviloutofit.sh
@@ -1,24 +1,39 @@
 #!/usr/bin/env bash
 
-FF_DIR="/usr/lib/firefox/browser"
-FH_DIR="/usr/lib/firefox-hardening/"
+FFH_DIR = "/usr/lib/firefox-hardening"
+FFH_CONFIG_DIR = "/etc/firefox-hardening"
 
-SYSTEM_ADDONS_DIR=${FF_DIR}/features
-SYSTEM_ADDONS="firefox@getpocket.com.xpi webcompat@mozilla.org.xpi aushelper@mozilla.org.xpi"
+FF_DIR = "/usr/lib/firefox"
+FF_SYS_ADDONS_DIR = "${FF_DIR}/browser/features"
 
-SYSTEM_PREFS_DIR=${FF_DIR}/defaults/preferences
-CUSTOM_CONFIGS="99-custom.js"
+if [ -f ${FFH_CONFIG_DIR}/ffh.conf ]; then
+	source ${FFH_CONFIG_DIR}/ffh.conf
+else
+	exit 1
+fi
 
 if [ -d ${FF_DIR} ]; then
-	for ADDON in ${SYSTEM_ADDONS}; do
-		if [ -f ${SYSTEM_ADDONS_DIR}/${ADDON} ]; then
-			rm ${SYSTEM_ADDONS_DIR}/${ADDON}
+	for ADDON in ${REMOVE_SYS_ADDONS}; do
+		if [ -f ${FF_SYS_ADDONS_DIR}/${ADDON} ]; then
+			rm ${FF_SYS_ADDONS_DIR}/${ADDON}
 		fi
 	done
 
-	for CONFIG in ${CUSTOM_CONFIGS}; do
-		if [ ! -L ${SYSTEM_PREFS_DIR}/${CONFIG} ]; then
-			ln -s ${FH_DIR}/prefs/${CONFIG} ${SYSTEM_PREFS_DIR}/${CONFIG}
+	echo "//" > ${FF_DIR}/custom.cfg
+
+	for CONFIG in ${ENABLED_FFH_CONFIGS}; do
+		if [ -f ${FFH_DIR}/prefs/${CONFIG} ]; then
+			cat ${FFH_DIR}/prefs/${CONFIG} >> ${FF_DIR}/custom.cfg
 		fi
 	done
+
+	for CONFIG in ${ENABLED_OWN_CONFIGS}; do
+		if [ -f ${FFH_CONFIG_DIR}/prefs/${CONFIG} ]; then
+			cat ${FFH_CONFIG_DIR}/prefs/${CONFIG} >> ${FF_DIR}/custom.cfg
+		fi
+	done
+
+	chmod 644 ${FF_DIR}/custom.cfg
+
+	ln -sf ${FFH_DIR}/prefs/autoconfig.js ${FF_DIR}/defaults/prefs/autoconfig.js
 fi
