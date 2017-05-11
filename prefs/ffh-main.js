5042c4583eafdd92bf283718e94780b694fa2adc
Felix Singer <singer.felix@t-online.de>
2017-05-11 22:28:07 +0200

    Fix tabs

--- prefs/ffh-main.js
+++ prefs/ffh-main.js
@@ -9,51 +9,51 @@ defaultPref("datareporting.healthreport.documentServerURI",		"");	// Hidden opti
 defaultPref("datareporting.policy.dataSubmissionEnabled",		false);
 
 // Crash reports
-defaultPref("breakpad.reportURL",								"");
-defaultPref("browser.crashReports.unsubmittedCheck.autoSubmit",	false);
-defaultPref("browser.crashReports.unsubmittedCheck.enabled",	false);
+defaultPref("breakpad.reportURL",					"");
+defaultPref("browser.crashReports.unsubmittedCheck.autoSubmit",		false);
+defaultPref("browser.crashReports.unsubmittedCheck.enabled",		false);
 defaultPref("browser.tabs.crashReporting.sendReport",			false);
 defaultPref("browser.tabs.crashReporting.requestEmail",			false);
 defaultPref("browser.tabs.crashReporting.includeURL",			false);
-defaultPref("browser.tabs.crashReporting.emailMe",				false);
+defaultPref("browser.tabs.crashReporting.emailMe",			false);
 defaultPref("dom.ipc.plugins.flash.subprocess.crashreporter.enabled",	false);
-defaultPref("dom.ipc.plugins.reportCrashURL",					false);
+defaultPref("dom.ipc.plugins.reportCrashURL",				false);
 
 // Telemetry
-defaultPref("toolkit.telemetry.enabled",						false);
-defaultPref("toolkit.telemetry.unified",						false);
-defaultPref("toolkit.telemetry.cachedClientID",					"");
-defaultPref("toolkit.telemetry.server",							"");
+defaultPref("toolkit.telemetry.enabled",				false);
+defaultPref("toolkit.telemetry.unified",				false);
+defaultPref("toolkit.telemetry.cachedClientID",				"");
+defaultPref("toolkit.telemetry.server",					"");
 
 // Telemetry experiments
-defaultPref("experiments.enabled",								false);
-defaultPref("experiments.activeExperiment",						false);
-defaultPref("experiments.supported",							false);
-defaultPref("experiments.manifest.uri",							"");
-defaultPref("network.allow-experiments",						false);
+defaultPref("experiments.enabled",					false);
+defaultPref("experiments.activeExperiment",				false);
+defaultPref("experiments.supported",					false);
+defaultPref("experiments.manifest.uri",					"");
+defaultPref("network.allow-experiments",				false);
 
 // Pocket
-defaultPref("extensions.pocket.enabled",						false);
-defaultPref("extensions.pocket.api",							"");	// Hidden option
-defaultPref("extensions.pocket.oAuthConsumerKey",				"");	// Hidden option
-defaultPref("extensions.pocket.site",							"");	// Hidden option
+defaultPref("extensions.pocket.enabled",				false);
+defaultPref("extensions.pocket.api",					"");	// Hidden option
+defaultPref("extensions.pocket.oAuthConsumerKey",			"");	// Hidden option
+defaultPref("extensions.pocket.site",					"");	// Hidden option
 
 // Suggested sites
-defaultPref("browser.newtabpage.enhanced",						false);
-defaultPref("browser.newtabpage.introShown",					true);
-defaultPref("browser.newtabpage.directory.ping",				"");
-defaultPref("browser.newtabpage.directory.source",				"");
+defaultPref("browser.newtabpage.enhanced",				false);
+defaultPref("browser.newtabpage.introShown",				true);
+defaultPref("browser.newtabpage.directory.ping",			"");
+defaultPref("browser.newtabpage.directory.source",			"");
 
 // Safe browsing
-defaultPref("browser.safebrowsing.malware.enabled",				false);
+defaultPref("browser.safebrowsing.malware.enabled",			false);
 defaultPref("browser.safebrowsing.phishing.enabled",			false);
 defaultPref("browser.safebrowsing.downloads.enabled",			false);
-defaultPref("browser.safebrowsing.downloads.remote.enabled",					false);
+defaultPref("browser.safebrowsing.downloads.remote.enabled",				false);
 defaultPref("browser.safebrowsing.downloads.remote.block_dangerous",			false);
 defaultPref("browser.safebrowsing.downloads.remote.block_dangerous_host",		false);
-defaultPref("browser.safebrowsing.downloads.remote.block_potentially_unwanted",	false);
-defaultPref("browser.safebrowsing.downloads.remote.block_uncommon",				false);
-defaultPref("browser.safebrowsing.downloads.remote.url",						"");
+defaultPref("browser.safebrowsing.downloads.remote.block_potentially_unwanted",		false);
+defaultPref("browser.safebrowsing.downloads.remote.block_uncommon",			false);
+defaultPref("browser.safebrowsing.downloads.remote.url",				"");
 
 // Google SafeBrowsing update URLs
 defaultPref("browser.safebrowsing.provider.google.updateURL",	"");
@@ -68,21 +68,21 @@ defaultPref("browser.safebrowsing.provider.mozilla.gethashURL",	"");
 // SafeBrowsing reporting URLs
 defaultPref("browser.safebrowsing.provider.google.reportURL",	"");
 defaultPref("browser.safebrowsing.provider.google4.reportURL",	"");
-defaultPref("browser.safebrowsing.reportMalwareMistakeURL",		"");
-defaultPref("browser.safebrowsing.reportPhishMistakeURL",		"");
-defaultPref("browser.safebrowsing.reportPhishURL",				"");
+defaultPref("browser.safebrowsing.reportMalwareMistakeURL",	"");
+defaultPref("browser.safebrowsing.reportPhishMistakeURL",	"");
+defaultPref("browser.safebrowsing.reportPhishURL",		"");
 
 // Heartbeat
-defaultPref("browser.selfsupport.enabled",						false);	// Hidden option
-defaultPref("browser.selfsupport.url",							"");
+defaultPref("browser.selfsupport.enabled",			false);	// Hidden option
+defaultPref("browser.selfsupport.url",				"");
 
 // WebIDE
-defaultPref("devtools.webide.enabled",							false);
-defaultPref("devtools.webide.autoinstallADBHelper",				false);
-defaultPref("devtools.webide.autoinstallFxdtAdapters",			false);
+defaultPref("devtools.webide.enabled",				false);
+defaultPref("devtools.webide.autoinstallADBHelper",		false);
+defaultPref("devtools.webide.autoinstallFxdtAdapters",		false);
 
 // Remote debugging
-defaultPref("devtools.debugger.remote-enabled",					false);
+defaultPref("devtools.debugger.remote-enabled",			false);
 
 
 
@@ -91,23 +91,23 @@ defaultPref("devtools.debugger.remote-enabled",					false);
  **************/
 
 // Extension updates
-defaultPref("extensions.update.enabled",						true);
+defaultPref("extensions.update.enabled",		true);
 
 // Extension block list
-defaultPref("extensions.blocklist.enabled",						true);
-defaultPref("extensions.blocklist.url",							"https://blocklist.addons.mozilla.org/blocklist/3/%APP_ID%/%APP_VERSION%/");
+defaultPref("extensions.blocklist.enabled",		true);
+defaultPref("extensions.blocklist.url",			"https://blocklist.addons.mozilla.org/blocklist/3/%APP_ID%/%APP_VERSION%/");
 
 // Extension metadata update
-defaultPref("extensions.getAddons.cache.enabled",				false);
+defaultPref("extensions.getAddons.cache.enabled",	false);
 
 // Click to play
-defaultPref("plugins.click_to_play",							true);
+defaultPref("plugins.click_to_play",			true);
 
 // Flash
-defaultPref("plugin.state.flash",								0);
+defaultPref("plugin.state.flash",			0);
 
 // Java
-defaultPref("plugin.state.java",								0);
+defaultPref("plugin.state.java",			0);
 
 // Gnome Shell integration
 defaultPref("plugin.state.libgnome-shell-browser-plugin",		0);	// Not found, probably only available if using Gnome Shell
@@ -119,28 +119,28 @@ defaultPref("plugin.state.libgnome-shell-browser-plugin",		0);	// Not found, pro
  ****************/
 
 // TLS versions
-defaultPref("security.tls.version.min",						1);
-defaultPref("security.tls.version.max",						4);
+defaultPref("security.tls.version.min",				1);
+defaultPref("security.tls.version.max",				4);
 
 // TLS version fallback
-defaultPref("security.tls.version.fallback-limit",			3);
+defaultPref("security.tls.version.fallback-limit",		3);
 
 // Session tickets
 defaultPref("security.ssl.disable_session_identifiers",		true);	// Hidden option
 
 // OCSP
-defaultPref("security.OCSP.enabled",						1);
+defaultPref("security.OCSP.enabled",				1);
 
 // OCSP stapling
-defaultPref("security.ssl.enable_ocsp_stapling",			true);
+defaultPref("security.ssl.enable_ocsp_stapling",		true);
 
 //
 defaultPref("security.cert_pinning.enforcement_level",		2);
 
 // SSL error reporting
-defaultPref("security.ssl.errorReporting.enabled",			false);
+defaultPref("security.ssl.errorReporting.enabled",		false);
 defaultPref("security.ssl.errorReporting.automatic",		false);
-defaultPref("security.ssl.errorReporting.url",				"");
+defaultPref("security.ssl.errorReporting.url",			"");
 
 
 
@@ -149,8 +149,8 @@ defaultPref("security.ssl.errorReporting.url",				"");
  ***********/
 
 // disable 1024-bit dh primes
-defaultPref("security.ssl3.dhe_rsa_aes_128_sha",			false);
-defaultPref("security.ssl3.dhe_rsa_aes_256_sha",			false);
+defaultPref("security.ssl3.dhe_rsa_aes_128_sha",		false);
+defaultPref("security.ssl3.dhe_rsa_aes_256_sha",		false);
 
 // RC4 fallback
 defaultPref("security.tls.unrestricted_rc4_fallback",		false);
@@ -169,22 +169,22 @@ defaultPref("dom.webnotifications.serviceworker.enabled",	false);
 defaultPref("dom.indexedDB.enabled",		true);
 
 // Gamepad
-defaultPref("dom.gamepad.enabled",			false);
+defaultPref("dom.gamepad.enabled",		false);
 
 // Vibrator
-defaultPref("dom.vibrator.enabled",			false);
+defaultPref("dom.vibrator.enabled",		false);
 
 // Virtual reality
-defaultPref("dom.vr.enabled",				false);
+defaultPref("dom.vr.enabled",			false);
 defaultPref("dom.vr.oculus.enabled",		false);
 defaultPref("dom.vr.openvr.enabled",		false);
-defaultPref("dom.vr.osvr.enabled",			false);
+defaultPref("dom.vr.osvr.enabled",		false);
 
 // Battery
-defaultPref("dom.battery.enabled",			false);
+defaultPref("dom.battery.enabled",		false);
 
 // Network information
-defaultPref("dom.netinfo.enabled",			false);
+defaultPref("dom.netinfo.enabled",		false);
 
 // Timing
 defaultPref("dom.enable_performance",		false);
@@ -199,47 +199,47 @@ defaultPref("dom.mozTCPSocket.enabled",		false);	// Hidden option
 defaultPref("media.peerconnection.enabled",	false);
 
 //
-defaultPref("media.peerconnection.turn.disable",					true);
+defaultPref("media.peerconnection.turn.disable",			true);
 
 // Hide internal IP
 defaultPref("media.peerconnection.ice.default_address_only",		true);
-defaultPref("media.peerconnection.ice.no_host",						true);
+defaultPref("media.peerconnection.ice.no_host",				true);
 
 //
-defaultPref("media.getusermedia.screensharing.enabled",				false);
+defaultPref("media.getusermedia.screensharing.enabled",			false);
 
 //
-defaultPref("media.getusermedia.audiocapture.enabled",				false);
+defaultPref("media.getusermedia.audiocapture.enabled",			false);
 
 //
-defaultPref("media.getusermedia.browser.enabled",					false);
+defaultPref("media.getusermedia.browser.enabled",			false);
 
 //
-defaultPref("media.peerconnection.video.enabled",					false);
+defaultPref("media.peerconnection.video.enabled",			false);
 
 // Speech synthesis
-defaultPref("media.webspeech.synth.enabled",						false);
+defaultPref("media.webspeech.synth.enabled",				false);
 
 // Speech recognition
 defaultPref("media.webspeech.recognition.enable",			false);
 
-defaultPref("media.navigator.enabled",						false);
+defaultPref("media.navigator.enabled",					false);
 defaultPref("media.navigator.video.enabled",				false);
 
-defaultPref("browser.send_pings",							false)
+defaultPref("browser.send_pings",					false)
 // Here will stand an option
 defaultPref("browser.send_pings.require_same_host",			true);
 
 // Beacons
-defaultPref("beacon.enabled",								false);
+defaultPref("beacon.enabled",						false);
 
 // WebGL
-defaultPref("webgl.disabled",									true);
-defaultPref("webgl.min_capability_mode",						true);
-defaultPref("webgl.disable-extensions",							true);
-defaultPref("webgl.disable-fail-if-major-performance-caveat",	true);
-defaultPref("webgl.enable-debug-renderer-info",					false);
-defaultPref("pdfjs.enableWebGL",								false);
+defaultPref("webgl.disabled",						true);
+defaultPref("webgl.min_capability_mode",				true);
+defaultPref("webgl.disable-extensions",					true);
+defaultPref("webgl.disable-fail-if-major-performance-caveat",		true);
+defaultPref("webgl.enable-debug-renderer-info",				false);
+defaultPref("pdfjs.enableWebGL",					false);
 
 
 
@@ -264,49 +264,49 @@ defaultPref("privacy.clearOnShutdown.siteSettings",		false);
 defaultPref("privacy.clearOnShutdown.openWindows",		false);
 
 // Manual clean
-defaultPref("privacy.cpd.cache",			true);
-defaultPref("privacy.cpd.cookies",			true);
+defaultPref("privacy.cpd.cache",		true);
+defaultPref("privacy.cpd.cookies",		true);
 defaultPref("privacy.cpd.downloads",		false);
-defaultPref("privacy.cpd.formdata",			false);
-defaultPref("privacy.cpd.history",			false);
+defaultPref("privacy.cpd.formdata",		false);
+defaultPref("privacy.cpd.history",		false);
 defaultPref("privacy.cpd.offlineApps",		false);
 defaultPref("privacy.cpd.passwords",		false);
-defaultPref("privacy.cpd.sessions",			false);
+defaultPref("privacy.cpd.sessions",		false);
 defaultPref("privacy.cpd.siteSettings",		false);
 defaultPref("privacy.cpd.openWindows",		false);
 
 // Private browsing mode
-defaultPref("browser.privatebrowsing.autostart",		false);
+defaultPref("browser.privatebrowsing.autostart",	false);
 
 // Offline cache
-defaultPref("browser.cache.offline.enable",				false);
+defaultPref("browser.cache.offline.enable",		false);
 
 // Disk cache
-defaultPref("browser.cache.disk.enable",				true);
+defaultPref("browser.cache.disk.enable",		true);
 
 // Memory cache
-defaultPref("browser.cache.memory.enable",				false);
+defaultPref("browser.cache.memory.enable",		false);
 
 //
-defaultPref("browser.cache.disk_cache_ssl",				false);
+defaultPref("browser.cache.disk_cache_ssl",		false);
 
 // Form autofill
-defaultPref("browser.formfill.enable",					false);
+defaultPref("browser.formfill.enable",			false);
 
 // Extra session data
-defaultPref("browser.sessionstore.privacy_level",		0);
+defaultPref("browser.sessionstore.privacy_level",	0);
 
 // History
-defaultPref("places.history.enabled",					true);
+defaultPref("places.history.enabled",			true);
 
 // Cookie lifetime
-defaultPref("network.cookie.lifetimePolicy",			2);
+defaultPref("network.cookie.lifetimePolicy",		2);
 
 //
-defaultPref("offline-apps.allow_by_default",			false);
+defaultPref("offline-apps.allow_by_default",		false);
 
 // Visited links
-defaultPref("layout.css.visited_links_enabled",			false);
+defaultPref("layout.css.visited_links_enabled",		false);
 
 
 
@@ -318,13 +318,13 @@ defaultPref("layout.css.visited_links_enabled",			false);
 defaultPref("camera.control.face_detection.enabled",	false);
 
 // Reading sensors
-defaultPref("device.sensors.enabled",					false);
+defaultPref("device.sensors.enabled",			false);
 
 // Geo location
-defaultPref("geo.enabled",						false);
-defaultPref("geo.wifi.uri",						"");
+defaultPref("geo.enabled",				false);
+defaultPref("geo.wifi.uri",				"");
 defaultPref("geo.wifi.logging.enabled",			false);	// Hidden option
-defaultPref("geo.wifi.xhr.timeout",				1);
+defaultPref("geo.wifi.xhr.timeout",			1);
 defaultPref("browser.search.geoip.url",			"");
 defaultPref("browser.search.geoip.timeout",		1);
 defaultPref("browser.search.geoSpecificDefaults",	false);
@@ -332,16 +332,16 @@ defaultPref("browser.search.geoSpecificDefaults.url",	"");
 
 
 // Copy selected contents to clipboard
-defaultPref("clipboard.autocopy",					false);
+defaultPref("clipboard.autocopy",			false);
 
 // Keyboard events
 defaultPref("dom.keyboardevent.code.enabled",		false);
 
 //
-defaultPref("keyword.enabled",						false);
+defaultPref("keyword.enabled",				false);
 
 //
-defaultPref("browser.urlbar.trimURLs",				false);
+defaultPref("browser.urlbar.trimURLs",			false);
 
 //
 defaultPref("browser.fixup.alternate.enabled",		false);
@@ -353,47 +353,47 @@ defaultPref("browser.tabs.closeWindowWithLastTab",	false);
 defaultPref("dom.event.contextmenu.enabled",		false);
 
 //
-defaultPref("browser.ctrlTab.previews",				true);
+defaultPref("browser.ctrlTab.previews",			true);
 
 //
-defaultPref("security.insecure_password.ui.enabled",		true);
+defaultPref("security.insecure_password.ui.enabled",	true);
 
 //
 defaultPref("general.buildID.override",			"20100101");
 
 //
-defaultPref("browser.display.use_document_fonts",		0);
+defaultPref("browser.display.use_document_fonts",	0);
 
 //
-defaultPref("privacy.resistFingerprinting",			true);
+defaultPref("privacy.resistFingerprinting",		true);
 
 // Social
 defaultPref("social.remote-install.enabled",		false);
 defaultPref("social.share.activationPanelEnabled",	false);
 defaultPref("social.toast-notifications.enabled",	false);
-defaultPref("social.shareDirectory",				"");
-defaultPref("social.whitelist",						"");
-defaultPref("social.directories",					"");
+defaultPref("social.shareDirectory",			"");
+defaultPref("social.whitelist",				"");
+defaultPref("social.directories",			"");
 defaultPref("social.sidebar.unload_timeout_ms",		1);
 
 
 // disable cookies from 3rd parties
-defaultPref("network.cookie.cookieBehavior",					1);
-defaultPref("signon.rememberSignons",							false);
+defaultPref("network.cookie.cookieBehavior",			1);
+defaultPref("signon.rememberSignons",				false);
 // enable Do Not Track
-defaultPref("privacy.donottrackheader.enabled",				true);
+defaultPref("privacy.donottrackheader.enabled",			true);
 // enable tracking protection
 // https://support.mozilla.org/t5/Protect-your-privacy/Tracking-Protection-in-Firefox/ta-p/18255
-defaultPref("privacy.trackingprotection.enabled",				true);
+defaultPref("privacy.trackingprotection.enabled",		true);
 // disable DNS defaultPrefetch
-defaultPref("network.dns.disablePrefetch",						true);
+defaultPref("network.dns.disablePrefetch",			true);
 // try resolving DNS over SOCKS proxies (needed for Tor)
-defaultPref("network.proxy.socks_remote_dns",					true);
+defaultPref("network.proxy.socks_remote_dns",			true);
 // disable search suggestions
-defaultPref("browser.search.suggest.enabled",					false);
+defaultPref("browser.search.suggest.enabled",			false);
 // do not embed the `missing flash player` frame
-defaultPref("plugins.notifyMissingFlash",						false);
+defaultPref("plugins.notifyMissingFlash",			false);
 // set the useragent to the same one used by the Tor browser
-defaultPref("general.useragent.override",						"Mozilla/5.0 (Windows NT 6.1; rv:38.0) Gecko/20100101 Firefox/38.0");
+defaultPref("general.useragent.override",			"Mozilla/5.0 (Windows NT 6.1; rv:38.0) Gecko/20100101 Firefox/38.0");
 // set the platform to the same one used by the Tor browser
-defaultPref("general.platform.override",						"Win32");
+defaultPref("general.platform.override",			"Win32");
